9b92b6c7b6265b83ac8191ccee4f04865ce246bc
Stealth <mr.duedull@gmail.com>
2025-06-28 22:32:57 +0530

    using arrayList examples

---
+++ src/Process/CollectionFramework/Main.java
@@ -0,0 +1,18 @@
+package Process.CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2,43,6,76,65,3,5,7};
+        
+        Iterator<Integer> iterator = Arrays.stream(arr).iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}

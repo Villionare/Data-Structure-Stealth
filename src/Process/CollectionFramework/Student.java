8a8289d8f717cfec3ff9c395e15263f8bfbe129c
Stealth <mr.duedull@gmail.com>
2025-06-29 16:18:04 +0530

    .

---
+++ src/Process/CollectionFramework/Student.java
@@ -0,0 +1,4 @@
+package Process.CollectionFramework;
+
+public class Student {
+}
---
+++ src/Process/CollectionFramework/Tester.java
@@ -0,0 +1,25 @@
+package Process.CollectionFramework;
+
+import java.util.ArrayList;
+
+public class Tester {
+    public static void main(String[] args) {
+        ArrayList<Student> student = new ArrayList<>();
+        student.add(new Student(2, "ritikk", true));
+        student.add(new Student(2, "abi", false));
+        student.add(new Student(2, "polo", true));
+        student.add(new Student(2, "kick", false));
+
+        ArrayList<String> studentNames = new ArrayList<String>();
+
+        for (Student i : student) {
+            studentNames.add(i.getStudentName());
+            System.out.println("Student name: " + i.getStudentName());
+            System.out.println("Student Id: " + i.getStudentId());
+            System.out.println("Course Registered: " + i.getCourseRegistered());
+        }
+
+        System.out.println(studentNames);
+
+    }
+}

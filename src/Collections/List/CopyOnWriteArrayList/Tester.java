ae6a74a9747e905697af0089941f0ce0c5e86f3f
Stealth <mr.duedull@gmail.com>
2025-07-04 20:09:41 +0530

    Operation on CopyOnWriteArrayList

---
+++ src/Collections/List/CopyOnWriteArrayList/Tester.java
@@ -0,0 +1,53 @@
+package Collections.List.CopyOnWriteArrayList;
+
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        //this is a special one - as it creates copy of the List when there is any modification going on
+        //when the modification gets done it changes the references to the new one
+        //- All mutative operations (add, set, remove, etc.) create a new copy of the underlying array.
+        //- Ideal when reads vastly outnumber writes.
+
+        CopyOnWriteArrayList<Integer> cowal = new CopyOnWriteArrayList<>();
+
+        cowal.addLast(287);
+        cowal.addLast(263);
+        cowal.addLast(239);
+        cowal.addLast(260);
+        cowal.addLast(234);
+        cowal.addLast(252);
+
+        //let's take the example of ArrayList
+        ArrayList<String> alist = new ArrayList<>();
+        alist.add("this");
+        alist.add("is");
+        alist.add("a");
+        alist.add("Array");
+        alist.add("List.");
+
+//        for (String item : alist){
+//            System.out.println(item);
+//
+//            if (item.equals("Array")){
+//                alist.add("yuuuu huuuu");
+//                //This will show an error as we can't modify a list concurrently
+//                //we can solve this issue by CopyOnWriteArrayList
+//            }
+//        }
+
+        for (Integer item : cowal){
+            System.out.println(item);
+
+            if (item.equals(260)){
+                int temp = cowal.indexOf(260);
+                cowal.add(++temp, 999);
+            }
+        }
+
+        System.out.println(cowal);
+    }
+}
--- src/Collections/List/Vector/Tester.java
+++ src/Collections/List/Vector/Tester.java
@@ -1,8 +1,10 @@
 package Collections.List.Vector;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
-public class Tester {
+public class Tester{
+
 
     public static void main(String[] args) {
         //is is just like arrayList, stores elements in continues dynamic array
@@ -23,8 +25,38 @@ public class Tester {
         System.out.println(v1);
         System.out.println(v1.capacity());
 
+        Vector<Integer> newVector = new Vector<>();
+        //in the case of Vector which is thread safe and it stores from one thread at a time
 
+        //ArrayList<Integer> newVector = new ArrayList<>();
+        //ArrayList is not Synchronized i.e. not thread safe
+        // while using arrayList you will notice that after inserting the elements concurrently
+        //the size is changing everytime due to the 2 threads working concurrently
 
+        Thread Thread1 = new Thread(()->{
+            for (int i = 1; i <= 1000; i++){
+                newVector.add(i);
+            }
+        });
 
+        Thread Thread2 = new Thread(()->{
+            for (int i = 1; i <= 1000; i++){
+                newVector.add(i);
+            }
+        });
+
+        Thread1.start();
+        Thread2.start();
+
+        try{
+            Thread1.join();
+            Thread2.join();
+        }catch (InterruptedException e){
+            System.out.println(e);
+        }
+
+        System.out.println(newVector.size());
     }
+
+
 }

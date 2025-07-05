25a8c7dbae1c859f76afa45a0a77ed6299319e13
Stealth <mr.duedull@gmail.com>
2025-07-05 22:23:08 +0530

    Maps progression

---
+++ src/Map/HashMap/Tester.java
@@ -0,0 +1,45 @@
+package Map.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Tester {
+    public static void main(String[] args) {
+
+        //Hashmap - !Synchronized = !thread-safe
+
+        Map<Integer, String> hash1 = new HashMap<>();
+        hash1.put(1, "abhay");
+        hash1.put(2, "abhishek");
+        hash1.put(3, "aditya");
+        hash1.put(null, "this is a null value");
+
+        System.out.println(hash1);
+
+        String value = hash1.get(3);
+        System.out.println(value);
+
+        System.out.println(hash1.containsValue("yo"));;
+        System.out.println(hash1.containsKey(56));;
+
+        //to loop through the map use set, it's a basic set like in mathematics
+        //this set will store all the key of the maps
+        //so then we can iterate through each of them:
+        Set<Integer> mapset = hash1.keySet();
+
+        for (Integer K_V: mapset){
+            System.out.println(K_V +" - "+ hash1.get(K_V));
+        }
+
+        //another method to loop through HashMap:
+        Set<Map.Entry<Integer, String>> another = hash1.entrySet();
+
+        for (Map.Entry<Integer, String> item : another){
+            System.out.println(item.getClass()+" - "+ item.getKey() + " : "+ item.getValue());
+        }
+
+        hash1.remove(null);
+        System.out.println(hash1);
+    }
+}

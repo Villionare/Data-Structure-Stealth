454fa8d8f235041657e9fd0d8538162b5b466169
Stealth <mr.duedull@gmail.com>
2025-07-15 19:13:27 +0530

    binary search algo

--- src/com/DsaPlaylist/BinarySearch/Main.java
+++ src/com/DsaPlaylist/BinarySearch/Main.java
@@ -9,17 +9,31 @@ public class Main {
 
         int[] arr = {1,2,3,4,5,6,7,8,9,10,34,44,54,64,74,84,94,100};
 
-        //to search = 94
-        int target = 94;
-
-        int length = (arr.length-1)/2;
-//        System.out.println(length);
-
-        if (target> arr[length]){
-            //right side
-
-        } else if (target<arr[length]) {
-            //left side
+        int result = binarySearch(arr, 100);
+        if (result >= 0){
+            System.out.printf("The element is found at index %d", result);
+        } else {
+            System.out.println("element is not found");
         }
+    }
+
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        //so now we need to find the middle index
+
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if (target > arr[mid]){
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (arr[mid] == target) {
+                return mid;
+            }
+        }
+        //if not found
+        return -1;
     }
 }

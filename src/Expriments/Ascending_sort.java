b36d548d4f4db71506962ca0d182503eda7f61b1
Stealth <mr.duedull@gmail.com>
2025-07-16 22:32:17 +0530

    created ascending order algoritem for array (while vibing)

---
+++ src/Expriments/Ascending_sort.java
@@ -0,0 +1,30 @@
+package Expriments;
+
+import java.util.Arrays;
+
+public class Ascending_sort {
+
+    public static void main(String[] args) {
+        //ascending order sort
+        int[] arr =  {34,67,340,934,765,23,78,893,4,78,312,7698,1};
+
+        arr = sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    private static int[] sort(int[] arr){
+
+        for (int i = 0; i< arr.length; i++){
+        int temp = 0;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] <= arr[i]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+}

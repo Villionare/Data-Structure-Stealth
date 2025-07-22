403491b63f750e32d99ff3df4b1942998d7d9da1
Stealth <mr.duedull@gmail.com>
2025-07-23 00:17:33 +0530

    finally did the rotated array question

--- src/com/DsaPlaylist/BinarySearch/Questions/RotatedSortedArray.java
+++ src/com/DsaPlaylist/BinarySearch/Questions/RotatedSortedArray.java
@@ -2,7 +2,7 @@ package com.DsaPlaylist.BinarySearch.Questions;
 
 //https://leetcode.com/problems/search-in-rotated-sorted-array/
 
-
+//old approach:
 //1. finding the peak
 //2. devide into increasing and decreasing and perform binary search
 //3. if exists in any side return [index]
@@ -122,13 +122,50 @@ package com.DsaPlaylist.BinarySearch.Questions;
 //}
 
 
-//choosing a new approach
+//choosing a new approach:
+//1. binary seach to find the pivoit(peak)
+//    case 1: if mid > mid + 1 
+//    case 2: if mid - 1  > mid
+//    case 3: if end > mid -> end = mid - 1
+//    case 4: if start < mid -> start = mid + 1
+//2. now when we have found the pivot then we have 2 ascending sorted arrays
+//3. do binary search on them one by one to check target
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end){
+        int mid = start + (end-start)/2;
+
+        if (target==nums[mid]){
+            return mid;
+        }
+
+        if (nums[start]<=nums[mid]){
+            if (nums[start]<=target && target<nums[mid]){
+                end = mid-1;
+            }else {
+                start = mid+1;
+            }
+        }else {
+            if (target>nums[mid]&&target<=nums[end]){
+                start = mid +1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        }
+        return -1;
+    }
+}
 
 public class RotatedSortedArray {
 
     public static void main(String[] args) {
-        int[] arr = {5,1,3};
-        int target = 3;
+        int[] arr = {1};
+        int target = 0;
 
         Solution start = new Solution();
 

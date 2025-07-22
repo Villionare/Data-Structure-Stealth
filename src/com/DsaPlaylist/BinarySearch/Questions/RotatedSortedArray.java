f7095a23f5431225204dab76d79869a59bc41bf5
Stealth <mr.duedull@gmail.com>
2025-07-22 22:29:26 +0530

    choosing a new approach

--- src/com/DsaPlaylist/BinarySearch/Questions/RotatedSortedArray.java
+++ src/com/DsaPlaylist/BinarySearch/Questions/RotatedSortedArray.java
@@ -8,123 +8,127 @@ package com.DsaPlaylist.BinarySearch.Questions;
 //3. if exists in any side return [index]
 //4. else return -1;
 
-class Solution {
-    public int search(int[] nums, int target) {
+//class Solution {
+//    public int search(int[] nums, int target) {
+//
+//        if (nums.length == 1){
+//            if (nums[0]==target){
+//                return 0;
+//            }else {
+//                return -1;
+//            }
+//        } else {
+//
+//            int peak = peak(nums);
+//
+//            if (peak == 0){
+//
+//                int rightSearch = binarySearch(nums,target,peak+1,nums.length-1);
+//                return rightSearch;
+//
+//            } else if (peak == nums.length-1) {
+//
+//                int leftSearch = binarySearch(nums,target,0, peak);
+//                return leftSearch;
+//
+//            }else {
+//
+//            int leftSearch = binarySearch(nums,target,0, peak);
+//
+//            if (leftSearch != -1){
+//                return leftSearch;
+//
+//            }else{
+//
+//                int rightSearch = binarySearch(nums,target,peak,nums.length-1);
+//
+//            if (rightSearch != -1){
+//
+//                    return rightSearch;
+//
+//                }else{
+//
+//                    return -1;
+//                }
+//            }
+//
+//            }
+//
+//        }
+//    }
+//
+//    static int binarySearch(int[] arr, int target, int start, int end) {
+//
+//        if (arr.length == 2){
+//            if (arr[0]==target){
+//                return 0;
+//            }else if (arr[1]==target){
+//                return 1;
+//            }else{
+//                return -1;
+//            }
+//        }else {
+//
+//        if (arr[start] < arr[end]) {
+//            // ascending
+//            while (start <= end) {
+//                int mid = start + (end - start) / 2;
+//
+//                if (target < arr[mid]) {
+//                    end = mid - 1;
+//                } else if (target > arr[mid]) {
+//                    start = mid + 1;
+//                } else {
+//                    return mid;
+//                }
+//            }
+//
+//        } else if (arr[start] > arr[end]) {
+//            // descending
+//            while (start <= end) {
+//                int mid = start + (end - start) / 2;
+//
+//                if (target < arr[mid]) {
+//                    start = mid + 1;
+//                } else if (target > arr[mid]) {
+//                    end = mid - 1;
+//                } else {
+//                    return mid;
+//                }
+//            }
+//        }
+//    }
+//        // No immediate return here; let the loop do the search.
+//        return -1;
+//    }
+//
+//    static int peak(int[] arr) {
+//        int start = 0;
+//        int end = arr.length - 1;
+//
+//        while (start < end) {
+//            int mid = start + (end - start) / 2;
+//            int midVal = arr[mid];
+//            int nextVal = arr[mid + 1];
+//
+//            if (midVal < nextVal) {
+//                start = mid + 1;
+//            } else {
+//                end = mid;
+//            }
+//        }
+//        return start; // or end, both point to the peak
+//    }
+//}
 
-        if (nums.length == 1){
-            if (nums[0]==target){
-                return 0;
-            }else {
-                return -1;
-            }
-        } else {
 
-            int peak = peak(nums);
-
-            if (peak == 0){
-
-                int rightSearch = binarySearch(nums,target,peak+1,nums.length-1);
-
-            } else if (peak == nums.length-1) {
-
-                int leftSearch = binarySearch(nums,target,0, peak);
-
-            }else {
-
-            int leftSearch = binarySearch(nums,target,0, peak);
-
-            if (leftSearch != -1){
-                return leftSearch;
-
-            }else{
-
-                int rightSearch = binarySearch(nums,target,peak+1,nums.length-1);
-
-            if (rightSearch != -1){
-
-                    return rightSearch;
-
-                }else{
-
-                    return -1;
-                }
-            }
-
-            }
-
-        }
-        return -1;
-    }
-
-    static int binarySearch(int[] arr, int target, int start, int end) {
-
-        if (arr.length == 2){
-            if (arr[0]==target){
-                return 0;
-            }else if (arr[1]==target){
-                return 1;
-            }else{
-                return -1;
-            }
-        }else {
-
-        if (arr[start] < arr[end]) {
-            // ascending
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-
-                if (target < arr[mid]) {
-                    end = mid - 1;
-                } else if (target > arr[mid]) {
-                    start = mid + 1;
-                } else {
-                    return mid;
-                }
-            }
-
-        } else if (arr[start] > arr[end]) {
-            // descending
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-
-                if (target < arr[mid]) {
-                    start = mid + 1;
-                } else if (target > arr[mid]) {
-                    end = mid - 1;
-                } else {
-                    return mid;
-                }
-            }
-        }
-    }
-        // No immediate return here; let the loop do the search.
-        return -1;
-    }
-
-    static int peak(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            int midVal = arr[mid];
-            int nextVal = arr[mid + 1];
-
-            if (midVal < nextVal) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        return start; // or end, both point to the peak
-    }
-}
+//choosing a new approach
 
 public class RotatedSortedArray {
 
     public static void main(String[] args) {
-        int[] arr = {1,3};
-        int target = 0;
+        int[] arr = {5,1,3};
+        int target = 3;
 
         Solution start = new Solution();
 

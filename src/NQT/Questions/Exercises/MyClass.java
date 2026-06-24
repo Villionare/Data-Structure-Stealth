87cd9566304e82eff0a37f5e454a6dd699d74151
ABHAY <121710071+Villionare@users.noreply.github.com>
2026-06-24 23:21:07 +0530

    diddy

--- src/NQT/Questions/Exercises/MyClass.java
+++ src/NQT/Questions/Exercises/MyClass.java
@@ -2,16 +2,30 @@ package NQT.Questions.Exercises;
 
 public class MyClass{
     public static void main(String[] args){
-//program to check weather a string is a substring of larger string or not
+//this program is to check weather a number is strong number or not
 
-        String s = "The is diddy";
-        String check = "diddy";
+        int no = 147;
+        int check = no;
+        int sum = 0;
 
-        if(s.contains(check)){
-        System.out.println("yes");
-        return;
+        while(no>0){
+            int last = no%10;
+            sum += factorial(last);
+            no /= 10;
+        }
+
+        if(check == sum){
+            System.out.println("Strong");
+        }else{
+            System.out.println("not");
+        }
     }
 
-System.out.println("not");
+    private static int factorial(int no){
+        int fact = 1;
+        for(int i=no; i>=1; i--){
+            fact *= i;
+        }
+        return fact;
     }
 }
